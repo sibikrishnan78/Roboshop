066993e00d78c71d7dd7d71e8517d994990db43d
sibi <sibisk78@gmail.com>
2023-12-13 22:45:33 +0530

    Frontend first pull - 2

--- Frontend.sh
+++ Frontend.sh
@@ -1,10 +1,8 @@
 dnf install nginx -y
-systemctl enable nginx
-systemctl start nginx
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-vim /etc/nginx/default.d/roboshop.conf
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
-systemctl restart nginx
+systemctl enable nginx
+systemctl start nginx

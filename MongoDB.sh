2b18e17e0688fd14242d8d4e90500e175d1c997d
sibi <sibisk78@gmail.com>
2023-12-14 13:28:02 +0530

    mongo first-2 pull

--- MongoDB.sh
+++ MongoDB.sh
@@ -2,6 +2,5 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
-vim /etc/mongod.conf
-sed 's|127.0.0.1|0.0.0.0|g'
+sed -i -e 's|127.0.0.1|0.0.0.0|g' /etc/mongod.conf
 systemctl restart mongod

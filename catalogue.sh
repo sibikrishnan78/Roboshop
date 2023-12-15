ae757d55d740746a445f8b183e26e424bcedf09e
sibi <sibisk78@gmail.com>
2023-12-15 13:42:39 +0530

    catalogue 7 pull

--- catalogue.sh
+++ catalogue.sh
@@ -9,10 +9,10 @@ cd /app
 unzip /tmp/catalogue.zip
 cd /app
 npm install
-cp /home/centos/Roboshop/mongo.repo /etc/systemd/system/catalogue.service
+cp Roboshop/mongo.repo /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
-cp /home/centos/Roboshop/mongo.repo /etc/yum.repos.d/mongo.repo
+cp Roboshop/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org-shell -y
 mongo --host mongodb.deveng23.online </app/schema/catalogue.js

5bc6d6c064837dd3ebcea79ed779c1d7726b46bc
sibi <sibisk78@gmail.com>
2023-12-15 22:46:30 +0530

    mysql 1

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,6 @@
 echo -e "\e[35m>>>>>>>>>disabling mysql<<<<<<<<<<\e[0m"
 dnf module disable mysql -y
-cp /home/centos/Roboshop/mysql.sh /etc/yum.repos.d/mysql.repo
+cp /home/centos/Roboshop/mysql.repo /etc/yum.repos.d/mysql.repo
 echo -e "\e[35m>>>>>>>>>install mysql<<<<<<<<<<\e[0m"
 dnf install mysql-community-server -y
 echo -e "\e[35m>>>>>>>>>restart<<<<<<<<<<\e[0m"

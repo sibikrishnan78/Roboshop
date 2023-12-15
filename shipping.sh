eda1531a5eb6a7e5cf2c52aabf5d10327420a050
sibi <sibisk78@gmail.com>
2023-12-15 23:43:59 +0530

    shipping

--- shipping.sh
+++ shipping.sh
@@ -1,16 +1,22 @@
+echo -e "\e[35m>>>>>>>>>Install maven<<<<<<<<<<\e[0m"
 dnf install maven -y
+echo -e "\e[35m>>>>>>>>>adding user<<<<<<<<<<\e[0m"
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+echo -e "\e[35m>>>>>>>>>unzipping shipping<<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/shipping.zip
-cd /app
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
+echo -e "\e[35m>>>>>>>>>copying service file<<<<<<<<<<\e[0m"
 cp /home/centos/Roboshop/shipping.service /etc/systemd/system/shipping.service
+echo -e "\e[35m>>>>>>>>>Restart<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable shipping
 systemctl start shipping
+echo -e "\e[35m>>>>>>>>>load schema<<<<<<<<<<\e[0m"
 dnf install mysql -y
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql
+mysql -h mysql.deveng23.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
 systemctl restart shipping

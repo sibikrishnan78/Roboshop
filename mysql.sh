fab28323fb93c796ff46c8ae2373686ebc67177b
sibi <sibisk78@gmail.com>
2023-12-15 22:39:51 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,11 @@
+echo -e "\e[35m>>>>>>>>>disabling mysql<<<<<<<<<<\e[0m"
 dnf module disable mysql -y
 cp /home/centos/Roboshop/mysql.sh /etc/yum.repos.d/mysql.repo
+echo -e "\e[35m>>>>>>>>>install mysql<<<<<<<<<<\e[0m"
 dnf install mysql-community-server -y
+echo -e "\e[35m>>>>>>>>>restart<<<<<<<<<<\e[0m"
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
+echo -e "\e[35m>>>>>>>>>user<<<<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
 mysql -uroot -pRoboShop@1

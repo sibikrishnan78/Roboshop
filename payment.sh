6ddc0d338cde116245dfb8a92f996633cbb48934
sibi <sibisk78@gmail.com>
2023-12-16 19:32:18 +0530

    rabbitmq

--- payment.sh
+++ payment.sh
@@ -1,12 +1,18 @@
+echo -e "\e[35m>>>>>>>>>Install python<<<<<<<<<<\e[0m"
 dnf install python36 gcc python3-devel -y
+echo -e "\e[35m>>>>>>>>>Useradd<<<<<<<<<<\e[0m"
 useradd roboshop
+rm -rf /app
 mkdir /app
+echo -e "\e[35m>>>>>>>>>payment service<<<<<<<<<<\e[0m"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+echo -e "\e[35m>>>>>>>>>unzip payment<<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/payment.zip
-cd /app
+echo -e "\e[35m>>>>>>>>>Install pip<<<<<<<<<<\e[0m"
 pip3.6 install -r requirements.txt
 cp /home/centos/Roboshop/payment.service /etc/systemd/system/payment.service
+echo -e "\e[35m>>>>>>>>>Restart<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable payment
 systemctl start payment
